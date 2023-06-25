bb722561916672b12b47296c501b684b5a2593ab
JacobLinCool <jacoblincool@gmail.com>
2023-06-26 02:26:58 +0800

    feat: add cache for SQLite connector

---
+++ .changeset/good-shirts-bow.md
@@ -0,0 +1,5 @@
+---
+"sveltekit-db": patch
+---
+
+Add cache for SQLite connector
--- src/lib/sqlite.ts
+++ src/lib/sqlite.ts
@@ -2,14 +2,22 @@ import type { Database as DBInstance } from "better-sqlite3";
 import { Kysely, SqliteDialect } from "kysely";
 import { SQLITE_FILE } from "./env.js";
 
+export const SQLiteDBCache = new Map<string, DBInstance>();
+
 export class SQLiteDB<Schema> extends Kysely<Schema> {
 	constructor() {
 		super({
 			dialect: new SqliteDialect({
 				database: async () => {
+					if (SQLiteDBCache.has(SQLITE_FILE)) {
+						return SQLiteDBCache.get(SQLITE_FILE) as DBInstance;
+					}
+
 					const lib = "better-sqlite3";
 					const { default: DB } = await import(/* @vite-ignore */ lib);
-					return new DB(SQLITE_FILE, { verbose: console.log }) as DBInstance;
+					const db = new DB(SQLITE_FILE, { verbose: console.log }) as DBInstance;
+					SQLiteDBCache.set(SQLITE_FILE, db);
+					return db;
 				},
 			}),
 		});
