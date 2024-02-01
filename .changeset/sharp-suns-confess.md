df2896ae7517558f369ba58e05b68c7bb58be44e
JacobLinCool <jacoblincool@gmail.com>
2024-02-02 05:54:11 +0800

    feat: remove d1 shim, add D1 binding env var

---
+++ .changeset/sharp-suns-confess.md
@@ -0,0 +1,5 @@
+---
+"sveltekit-db": minor
+---
+
+Remove D1 shim, select D1 database with env var `D1`, `DB`, and `DATABASE`
--- src/lib/cloudflare-d1/index.ts
+++ src/lib/cloudflare-d1/index.ts
@@ -1,21 +1,25 @@
 import { env } from "$env/dynamic/private";
+import type { D1Database } from "@cloudflare/workers-types";
 import { Kysely } from "kysely";
 import { D1Dialect } from "kysely-d1";
-import { D1Database } from "./shim.js";
 
 export class CloudflareD1<Schema> extends Kysely<Schema> {
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	constructor() {
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		const db = (env.D1 || env.__D1_BETA__D1) as any;
+		const db = (env.D1 || env.DB || env.DATABASE) as unknown as D1Database;
 		if (!db) {
 			throw new Error("D1 database not available");
+		}
+
+		if (typeof db !== "object") {
+			throw new Error("Wrong type for D1 binding");
 		}
 
 		super({
 			// @ts-expect-error - types are wrong
 			// eslint-disable-next-line @typescript-eslint/no-explicit-any
-			dialect: new D1Dialect({ database: "fetch" in db ? (new D1Database(db) as any) : db }),
+			dialect: new D1Dialect({ database: db }),
 		});
 	}
 }
